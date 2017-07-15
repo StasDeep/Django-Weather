71aba43c67aaa33fd2a354296d0b0e6d8973e2ee
stasdeep <glubokiy.stas@gmail.com>
2017-07-15 15:33:16 +0300

    Change response from service.py

--- weather/service.py
+++ weather/service.py
@@ -6,7 +6,39 @@ WEATHER_API_KEY = '7ec667bf669cce3a71b5381e2fcdbf75'
 
 
 def weather_by_city_id(city_id):
+    """Take weather from Weather API and return it.
+    
+    Args:
+        city_id (str): unique number, representing city.
+        
+    Returns:
+        dict: object with information about weather.
+              Has keys:
+              - city_name (str),
+              - country_code (str),
+              - weather (str),
+              - weather_description (str),
+              - temperature (float),
+              - humidity (int),
+              - pressure (int),
+              - wind_speed (int),
+              - icon_url (str)
+            
+    """
     url_template = 'http://api.openweathermap.org/data/2.5/weather?id={}&appid={}'
     url = url_template.format(city_id, WEATHER_API_KEY)
     response = urllib2.urlopen(url).read()
-    return json.loads(response)
+    data = json.loads(response)
+
+    city_weather = dict()
+    city_weather['city_name'] = data['name']
+    city_weather['country_code'] = data['sys']['country']
+    city_weather['weather'] = data['weather'][0]['main']
+    city_weather['weather_description'] = data['weather'][0]['description']
+    city_weather['temperature'] = data['main']['temp'] - 273.15
+    city_weather['humidity'] = data['main']['humidity']
+    city_weather['pressure'] = data['main']['pressure']
+    city_weather['wind_speed'] = data['wind']['speed']
+    city_weather['icon_url'] = 'http://openweathermap.org/img/w/{}.png'.format(data['weather'][0]['icon'])
+
+    return city_weather
