6983a7abd166cfb46b18d321ac13053b497ce103
stasdeep <glubokiy.stas@gmail.com>
2017-07-15 14:40:25 +0300

    Create search form

--- weather/urls.py
+++ weather/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from weather.views import HomeView, CityWeatherView
+from weather.views import HomeView, CityWeatherView, CitySearchListView
 
+app_name = 'weather'
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^search$', CitySearchListView.as_view(), name='search_results'),
     url(r'^(?P<city_id>\d+)$', CityWeatherView.as_view(), name='city_weather')
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.views.generic import ListView
 from django.views.generic.base import TemplateView
 
+from weather.models import City
 from weather.service import weather_by_city_id
 
 
 class HomeView(TemplateView):
     template_name = 'weather/home.html'
+
+
+class CitySearchListView(ListView):
+    model = City
+
+    def get_queryset(self):
+        city_name = self.request.GET.get('q')
+        return self.model.objects.filter(name__istartswith=city_name)
+
+    def get_context_data(self, **kwargs):
+        context = super(CitySearchListView, self).get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
 
 
 class CityWeatherView(TemplateView):
