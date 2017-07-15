6d8adcbd295e70c52bc6ca26cde48967225c902e
stasdeep <glubokiy.stas@gmail.com>
2017-07-15 12:54:28 +0300

    Create models: City and Country

--- weather/models.py
+++ weather/models.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    code = models.CharField(max_length=2, primary_key=True)
+    name = models.CharField(max_length=255)
+
+
+class City(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(Country)
