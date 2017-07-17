c650cd3afaad299afdbe5b65ef5daca5e3077774
stasdeep <glubokiy.stas@gmail.com>
2017-07-17 15:30:28 +0300

    Add pagination to CitySearchListView

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views.generic.base import TemplateView
 
@@ -15,6 +16,16 @@ class HomeView(TemplateView):
 
 class CitySearchListView(ListView):
     model = City
+    paginate_by = 10
+
+    def get(self, request, *args, **kwargs):
+        try:
+            return super(CitySearchListView, self).get(request, *args, **kwargs)
+        except Http404:
+            response = redirect('weather:search_results')
+            params = '?q={q};page={page}'.format(q=request.GET.get('q'), page=1)
+            response['Location'] += params
+            return response
 
     def get_queryset(self):
         city_name = self.request.GET.get('q')
@@ -23,7 +34,35 @@ class CitySearchListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(CitySearchListView, self).get_context_data(**kwargs)
         context['q'] = self.request.GET.get('q')
+
+        self.trim_pagination(context)
+
         return context
+
+    def trim_pagination(self, context):
+        page = self.request.GET.get('page')
+
+        if page is None:
+            page = '1'
+
+        if page.isdigit():
+            page_num = int(page)
+            page_start = page_num - 3
+            page_finish = page_num + 3
+            last_page = context['paginator'].num_pages
+
+            if page_start <= 1:
+                page_finish += 1 - page_start
+                page_start = 1
+            else:
+                context['first'] = 1
+
+            if page_finish >= last_page:
+                page_finish = last_page
+            else:
+                context['last'] = last_page
+
+            context['page_range'] = xrange(page_start, page_finish + 1)
 
 
 class CityWeatherView(TemplateView):
